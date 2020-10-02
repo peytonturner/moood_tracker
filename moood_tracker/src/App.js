d42fda2ad656aa303214e25b12d9e16c6527fc65
peytont243 <peytont243@gmail.com>
2020-10-01 23:37:06 -0500

    set up router

--- moood_tracker/src/App.js
+++ moood_tracker/src/App.js
@@ -1,27 +1,54 @@
 import React from 'react';
 import styled from "styled-components";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Login from './auth.js';
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppDiv>
+      <Router>
+        <Route exact path="/" component={() => <Redirect to="/app" />} />
+        <Route path="/app" component={Main} />
+        <Route path="/login" component={Login} />
+      </Router>
+    </AppDiv>
   );
 }
 
-export default App;
+const AppDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = ({}) => {
+
+  return (
+    <div>This is main</div>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code>
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
